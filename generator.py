eb5d8e658bffb2cbafc9e52c3a2f88e095621859
Hykilpikonna <me@hydev.org>
2021-12-29 12:03:00 -0500

    [-] Remove baseDir property

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         if os.path.isfile(info_path):
             with io.open(info_path, 'r', encoding='utf-8') as f:
                 o = json5.load(f)
-                output.append({'path': p, 'id': o['id'], 'name': o['name'], 'profileUrl': o['profileUrl'], 'baseUrl': f'/people/{p}'})
+                output.append({'path': p, 'id': o['id'], 'name': o['name'], 'profileUrl': o['profileUrl']})
 
     Path("generated").mkdir(parents=True, exist_ok=True)
     with io.open('generated/people-list.json5', 'w', encoding='utf-8') as f:
