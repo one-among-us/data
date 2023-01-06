327e4b6347f756daaa931aae49fb1ba9bca62c99
Azalea Gui <me@hydev.org>
2023-01-06 01:21:20 -0500

    [+] Preview build script

---
+++ scripts/preview.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd dist
+
+# Download web tarball
+curl -L "https://github.com/one-among-us/web/tarball/gh-pages" --output web.tgz
+
+# Extract tarball
+tar -xf web.tgz --strip-components=1
+
+# Replace data host url to "/"
+sed -i 's/"https:\/\/data.one-among.us"/window.location.origin/g' ./**/*.js
