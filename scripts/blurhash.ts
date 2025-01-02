28cf77ce77fc6244d265addfc8ded7d3dd6086bf
Elihuso Quigley <Elihuso@outlook.com>
2025-01-02 15:16:19 +0800

    [O] Use smaller image component

--- scripts/blurhash.ts
+++ scripts/blurhash.ts
@@ -3,5 +3,5 @@ import { getSync } from '@andreekeberg/imagedata';
 
 export async function encodeBlur(path: string): Promise<string> {
     const data = getSync(path);
-    return encode(data.data, data.width, data.height, 4, 4);
+    return encode(data.data, data.width, data.height, 2, 2);
 }
